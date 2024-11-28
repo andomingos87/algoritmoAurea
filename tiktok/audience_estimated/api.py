4d5f29c1bb2640938fa9d89617a001f04eee84d2
Anderson Domingos <andomingos87@gmail.com>
2024-11-28 10:34:00 +0000

    Correção da api do tiktok

--- tiktok/audience_estimated/api.py
+++ tiktok/audience_estimated/api.py
@@ -1,26 +1,30 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import requests
 
 app = FastAPI()
 
+# Define o modelo para os dados esperados na requisição
+class AudienceEstimateRequest(BaseModel):
+    advertiser_id: str
+    placement_type: str
+    objective_type: str
+    optimization_goal: str
+    location_ids: list
+    auto_targeting_enabled: bool
+
 @app.post("/estimate_audience/")
-async def estimate_audience():
+async def estimate_audience(request_data: AudienceEstimateRequest):
     # Endpoint e token de acesso
     url = "https://business-api.tiktok.com/open_api/v1.3/ad/audience_size/estimate/"
     headers = {
         "Access-Token": "85d5230c0ca61ef5f741e1ea7aa045e49e895740",
         "Content-Type": "application/json"
     }
-    # Dados da requisição
-    data = {
-        "advertiser_id": "7420919198131732481",
-        "placement_type": "PLACEMENT_TYPE_AUTOMATIC",
-        "objective_type": "REACH",
-        "optimization_goal": "CLICK",
-        "location_ids": ["6252001"],
-        "auto_targeting_enabled": True
-    }
-    
+
+    # Converte os dados recebidos para um dicionário
+    data = request_data.dict()
+
     # Fazendo a chamada para o endpoint
     try:
         response = requests.post(url, headers=headers, json=data)
